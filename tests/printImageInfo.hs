cbec8c13eb94d8f232a99e224b4df2557d64f731
Anders Leino <anders.leino@gmail.com>
2014-01-30 22:44:31 +0200

    added a thing that prints image info

---
+++ tests/printImageInfo.hs
@@ -0,0 +1,14 @@
+import Control.Applicative ((<$>), pure)
+import System.Environment (getArgs)
+import Codec.Image.XCF
+import qualified Data.Attoparsec as Attoparsec
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Char8 as CharString
+
+printPartial (Attoparsec.Partial cont) = print $ cont $ CharString.pack ""
+
+main =
+  head <$> getArgs >>=
+  ByteString.readFile >>=
+  pure . parse >>=
+  print

a84487e45ee44000101bc76213641069f5ecb503
Anders Leino <anders.leino@gmail.com>
2014-01-31 11:03:49 +0200

    added a printlayers program

---
+++ tests/printLayers.hs
@@ -0,0 +1,17 @@
+import Control.Applicative ((<$>), pure)
+import System.Environment (getArgs)
+import Codec.Image.XCF
+import Codec.Image.XCF.Data.Image
+import qualified Data.Attoparsec as Attoparsec
+import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Char8 as CharString
+
+printPartial (Attoparsec.Partial cont) = print $ cont $ CharString.pack ""
+
+printLayerAtPointer = undefined
+
+main =
+  head <$> getArgs >>= ByteString.readFile >>= \bs -> do
+    (pure $ parse bs) >>=  \(Attoparsec.Done _ img) -> do
+      mapM_ print [let (Attoparsec.Done _ layer) = parseLayerAt lp bs in layer
+                  | lp <- layerPointers img]

f940a3329f7a39c4e071b070957b4f9613044c58
Anders Leino <anders.leino@gmail.com>
2014-02-16 15:33:04 +0200

    added some functions for decoding single tiles

--- Codec/Image/XCF/Data/Tile.hs
+++ Codec/Image/XCF/Data/Tile.hs
@@ -6,16 +6,35 @@ module Codec.Image.XCF.Data.Tile
        where
 
 import Data.Word
-import Data.ByteString
+import qualified Data.ByteString as ByteString
 import Prelude hiding (length)
 
-data Run = Run Int Word8 | Block ByteString deriving Show
+data Run = Run Int Word8 | Block ByteString.ByteString deriving Show
 
 data Tiles =
-  RawTiles ByteString | 
+  RawTiles ByteString.ByteString | 
   RGBTiles [([Run], [Run], [Run])] |
   RGBAlphaTiles [([Run], [Run], [Run], [Run])] |
   GrayscaleTiles [[Run]] |
   GrayscaleAlphaTiles [([Run], [Run])] |
   IndexedTiles [[Run]] |
   IndexedAlphaTiles [([Run], [Run])] deriving Show
+
+decodeRun :: Run -> ByteString.ByteString
+decodeRun (Run n b) = ByteString.replicate n b
+decodeRun (Block bs) = bs
+
+intertwine :: [[Run]] -> ByteString.ByteString
+intertwine = ByteString.concat . ByteString.transpose . map decodeTile1bpp
+
+decodeTile1bpp :: [Run] -> ByteString.ByteString
+decodeTile1bpp = ByteString.concat . map decodeRun
+
+decodeTile2bpp :: ([Run], [Run]) -> ByteString.ByteString
+decodeTile2bpp (runs1, runs2) = intertwine [runs1, runs2]
+
+decodeTile3bpp :: ([Run], [Run], [Run]) -> ByteString.ByteString
+decodeTile3bpp (runs1, runs2, runs3) = intertwine [runs1, runs2, runs3]
+
+decodeTile4bpp :: ([Run], [Run], [Run], [Run]) -> ByteString.ByteString
+decodeTile4bpp (runs1, runs2, runs3, runs4) = intertwine [runs1, runs2, runs3, runs4]

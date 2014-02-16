4ff7dd786e09c68f2105059e68588fc06cf8ca91
Anders Leino <anders.leino@gmail.com>
2014-02-16 17:25:46 +0200

    added function for decoding tiles (does not untangle tiles!)

--- Codec/Image/XCF/Data/Tile.hs
+++ Codec/Image/XCF/Data/Tile.hs
@@ -2,6 +2,7 @@ module Codec.Image.XCF.Data.Tile
        (
          Run (..),
          Tiles (..),
+         decodeTiles
        )
        where
 
@@ -38,3 +39,12 @@ decodeTile3bpp (runs1, runs2, runs3) = intertwine [runs1, runs2, runs3]
 
 decodeTile4bpp :: ([Run], [Run], [Run], [Run]) -> ByteString.ByteString
 decodeTile4bpp (runs1, runs2, runs3, runs4) = intertwine [runs1, runs2, runs3, runs4]
+
+decodeTiles :: Tiles -> ByteString.ByteString
+decodeTiles (RawTiles bs) = bs
+decodeTiles (RGBTiles rgbRuns) = ByteString.concat $ map decodeTile3bpp rgbRuns
+decodeTiles (RGBAlphaTiles rgbaRuns) = ByteString.concat $ map decodeTile4bpp rgbaRuns
+decodeTiles (GrayscaleTiles wRuns) = ByteString.concat $ map decodeTile1bpp wRuns
+decodeTiles (GrayscaleAlphaTiles waRuns) = ByteString.concat $ map decodeTile2bpp waRuns
+decodeTiles (IndexedTiles iRuns) = ByteString.concat $ map decodeTile1bpp iRuns
+decodeTiles (IndexedAlphaTiles iaRuns) = ByteString.concat $ map decodeTile2bpp iaRuns

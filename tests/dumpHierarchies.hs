d5d6a80b113f1de18f7a67e5621353e6f8a29e2b
Anders Leino <anders.leino@gmail.com>
2014-02-21 23:09:47 +0200

    fixed examples to get the color modes

--- tests/dumpHierarchies.hs
+++ tests/dumpHierarchies.hs
@@ -7,6 +7,7 @@ import Codec.Image.XCF.Data.Layer (hierarchyPointer)
 import qualified Codec.Image.XCF.Data.Hierarchy as Hierarchy
 import Codec.Image.XCF.Data.Level
 import Codec.Image.XCF.Data.Tile
+import qualified Codec.Image.XCF.Data.ColorMode as ColorMode
 import qualified Codec.Image.XCF.Data.Property as Property
 import qualified Data.Attoparsec as Attoparsec
 import qualified Data.ByteString as ByteString
@@ -17,6 +18,8 @@ main =
   head <$> getArgs >>= ByteString.readFile >>= \bs -> do
     (pure $ parse bs) >>=  \(Attoparsec.Done _ img) -> do
       let compressionIndicator = fromJust $ Image.compressionIndicator img
+          colorModeOpaque = Image.colorMode img
+          colorMode = ColorMode.NoAlpha colorModeOpaque          
           layers = [
             let (Attoparsec.Done _ layer) = parseLayerAt lp bs in layer
             | lp <- Image.layerPointers img
@@ -29,19 +32,18 @@ main =
             | lp <- map Hierarchy.levelPointer hierarchies
             ]
           tiless = [
-            let (Attoparsec.Done _ tiles) = parseTiles compressionIndicator level bs in tiles
+            let (Attoparsec.Done _ tiles) = parseTiles compressionIndicator colorMode level bs in tiles
             | level <- levels
             ]
-      sequence_ [do
-                    let width = Hierarchy.width $ hierarchy
-                        height = Hierarchy.height $ hierarchy
-                        bs = decodeTiles width height tiles
-                        bmp = packRGBA32ToBMP width height bs 
-                        filename = concat ["layer_", show i, ".bmp"]
-                    print "width: "
-                    print width
-                    print "height: "
-                    print height
-                    writeBMP filename bmp
-                | (hierarchy, tiles, i) <- zip3 hierarchies tiless [0 ..]
-                ]
+      sequence_ [
+        do
+          let width = Hierarchy.width $ hierarchy
+              height = Hierarchy.height $ hierarchy
+              bs = decodeTiles width height tiles
+              bmp = packRGBA32ToBMP width height bs 
+              filename = concat ["layer_", show i, ".bmp"]
+          putStrLn "width: " >> print width
+          putStrLn "height: " >> print height
+          writeBMP filename bmp
+        | (hierarchy, tiles, i) <- zip3 hierarchies tiless [0 ..]
+        ]
--- tests/printTiles.hs
+++ tests/printTiles.hs
@@ -7,6 +7,7 @@ import Codec.Image.XCF.Data.Layer (hierarchyPointer)
 import qualified Codec.Image.XCF.Data.Hierarchy as Hierarchy
 import Codec.Image.XCF.Data.Level
 import Codec.Image.XCF.Data.Tile
+import qualified Codec.Image.XCF.Data.ColorMode as ColorMode
 import qualified Codec.Image.XCF.Data.Property as Property
 import qualified Data.Attoparsec as Attoparsec
 import qualified Data.ByteString as ByteString
@@ -17,6 +18,8 @@ main =
   head <$> getArgs >>= ByteString.readFile >>= \bs -> do
     (pure $ parse bs) >>=  \(Attoparsec.Done _ img) -> do
       let compressionIndicator = fromJust $ Image.compressionIndicator img
+          colorModeOpaque = Image.colorMode img
+          colorMode = ColorMode.NoAlpha colorModeOpaque
           layers = [
             let (Attoparsec.Done _ layer) = parseLayerAt lp bs in layer
             | lp <- Image.layerPointers img
@@ -29,7 +32,9 @@ main =
             | lp <- map Hierarchy.levelPointer hierarchies
             ]
           tiless = [
-            let (Attoparsec.Done _ tiles) = parseTiles compressionIndicator level bs in tiles
+            parseTiles compressionIndicator colorMode level bs
+            --let (Attoparsec.Done _ tiles) = parseTiles compressionIndicator level bs in tiles
             | level <- levels
             ]
+      print compressionIndicator
       mapM_ print tiless
